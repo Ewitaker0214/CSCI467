de7436e7b0b65402be649b2b7116caf53086f0e5
Treyvon Whitaker <Ewhitaker0214@gmail.con>
2020-12-03 18:37:12 -0600

    Update order.php

--- GroupProject/pages/order.php
+++ GroupProject/pages/order.php
@@ -21,20 +21,7 @@
     echo "Connection to database failed: " . $e->getMessage();
   }
 
-  $username = 'z1845428';
-  $password = '2000Jan13';
-  $connected = false;
-  try { // if something goes wrong, an exception is thrown
-    $dsn = "mysql:host=courses;dbname=z1845428";
-    $pdo = new PDO($dsn, $username, $password);
-    $connected = true;
-  }
-  catch(PDOexception $e) { // handle that exception
-    echo "Connection to database failed: " . $e->getMessage();
-  }
 
-  $rs = $pdo_legacy->query("DESCRIBE parts;");
-  print_r($rs->fetchALL(PDO::FETCH_ASSOC));
 ?>
 
 <header>
@@ -45,25 +32,22 @@
 
 <body>
   <main id="">
-    </div>
+    <div>
     <form method="POST" action="./product_catalog.php?action=add#shopping_cart">
-      <tr>
-        <td># <?php echo $row["number"]; ?></td>
-        <input type="hidden" name="number" value="<?php echo $row["number"]; ?>"/>
-        <td><img src="<?php echo $row["pictureURL"]; ?>" alt="Image of <?php echo $row["description"]; ?>"/></td>
-        <td><?php echo $row["description"]; ?></td>
-        <input type="hidden" name="description" value="<?php echo $row["description"]; ?>"/>
-        <td>$<?php echo $row["price"]; ?></td>
-        <input type="hidden" name="price" value="<?php echo $row["price"]; ?>"/>
-        <td><input type="text" name="quantity" value=0 /></td>
-        <td><input type="submit" name="add_to_cart" value="Add to Cart"/></td>
-      </tr>
+        <label for="name">Name: </label>
+        <input type="text" name="name" value="<?php echo $name; ?>" required/><span style="color:red">* <?php echo $ErrName; ?></span>
+        <label for="email">Email: </label>
+        <input type="text" name="email" value="<?php echo $email; ?>" required/><span style="color:red">* <?php echo $ErrEmail; ?></span>
+        <label for="address">Address: </label>
+        <input type="text" name="address" value="<?php echo $address; ?>"/>
+        <input type="submit" name="submit" value="Submit"/>
       </form>
+    </div>
   </main>
 </body>
 
 <footer>
-  <p>Created by the Wuhan Clan for NIU CSCI466 Group Project &copy; 4/20/2020</p>
+  <p>Created by Group9A for NIU CSCI467 Group Project &copy; 12/04/2020</p>
 </footer>
 
 </html>
