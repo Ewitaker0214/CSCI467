4a9a80aaae27a7b2871190b87ac3f2509945ec8d
NateCode13 <75456877+NateCode13@users.noreply.github.com>
2020-12-03 22:15:35 -0600

    Update administration.php

--- GroupProject/pages/administration.php
+++ GroupProject/pages/administration.php
@@ -130,7 +130,7 @@ $SBD = "Select * From Order_History Where date_ordered <= " . $_POST["eDate"] . 
 $SBS = "Select * From Order_History Where shipped = " .$_POST["isShipped"]. "";
 $SBP = "Select * From Order_History Where purchase_amount <= " . $_POST["ePrice"] . " AND purchase_amount >= " . $_POST["sPrice"] ."" ;
 $SBA = "Select * From Order_History Where authorized = " .$_POST["isAuthorized"]. "";
-?>
+	      
 switch (true) 
 {
 	case isset($_POST["submit"]):
