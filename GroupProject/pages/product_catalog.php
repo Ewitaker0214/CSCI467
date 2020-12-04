6d8e5cf3382a12ebd961c1df9b56979012f62298
Treyvon Whitaker <Ewhitaker0214@gmail.con>
2020-12-04 15:52:07 -0600

    Update product_catalog.php

--- GroupProject/pages/product_catalog.php
+++ GroupProject/pages/product_catalog.php
@@ -123,10 +123,8 @@
         $rows1 = $rs1->fetchAll(PDO::FETCH_ASSOC);
 
         $rs2 = $pdo->query("SELECT part_number, description, in_stock FROM Product;");
-        if ($rs2 == 0)
-        {
-          $rows2 = $rs2->fetchAll(PDO::FETCH_ASSOC);
-        }
+        $rows2 = $rs2->fetchAll(PDO::FETCH_ASSOC);
+
         foreach ($rows1 as $row1)
         {
       ?>
@@ -139,8 +137,7 @@
           <input type="hidden" name="description" value="<?php echo $row1["description"]; ?>"/>
           <td>$<?php echo $row1["price"]; ?></td>
           <input type="hidden" name="price" value="<?php echo $row1["price"]; ?>"/>
-          <td><?php if ($rs2 == 0){foreach ( $rows2 as $row2 ) {if(row2["part_number"] == row1["number"] && row2["description"] == row1["description"]) echo $row2["in_stock"];}}
-                    else echo "0"?></td>
+          <td><?php foreach ( $rows2 as $row2 ) {if(row2["part_number"] == row1["number"] && row2["description"] == row1["description"]) echo $row2["in_stock"];}?></td>
           <td><input type="text" name="quantity" value=0 /></td>
           <td><input type="submit" name="add_to_cart" value="Add to Cart"/></td>
         </tr>
