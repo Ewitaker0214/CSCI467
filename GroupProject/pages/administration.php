200d3984aba54d725c429e0926dca3cab22d5505
NateCode13 <75456877+NateCode13@users.noreply.github.com>
2020-12-03 21:51:07 -0600

    Update administration.php

--- GroupProject/pages/administration.php
+++ GroupProject/pages/administration.php
@@ -154,7 +154,7 @@ switch (true)
 	foreach ($rows as $row)
         {
         ?>
-        <form method="POST" action="./product_catalog.php?action=add#shopping_cart">
+        <form method="POST" action="administration.php">
           <tr>
             <td># <?php echo $row["customer_id"]; ?></td>
             <input type="hidden" name="customer_id" value="<?php echo $row["customer_id"]; ?>"/>
@@ -194,7 +194,7 @@ switch (true)
 	foreach ($rows as $row)
         {
         ?>
-        <form method="POST" action="./product_catalog.php?action=add#shopping_cart">
+        <form method="POST" action="administration.php">
           <tr>
             <td># <?php echo $row["customer_id"]; ?></td>
             <input type="hidden" name="customer_id" value="<?php echo $row["customer_id"]; ?>"/>
@@ -234,7 +234,7 @@ switch (true)
 	foreach ($rows as $row)
         {
         ?>
-        <form method="POST" action="./product_catalog.php?action=add#shopping_cart">
+        <form method="POST" action="administration.php">
           <tr>
             <td># <?php echo $row["customer_id"]; ?></td>
             <input type="hidden" name="customer_id" value="<?php echo $row["customer_id"]; ?>"/>
@@ -274,7 +274,7 @@ switch (true)
 	foreach ($rows as $row)
         {
         ?>
-        <form method="POST" action="./product_catalog.php?action=add#shopping_cart">
+        <form method="POST" action="administration.php">
           <tr>
             <td># <?php echo $row["customer_id"]; ?></td>
             <input type="hidden" name="customer_id" value="<?php echo $row["customer_id"]; ?>"/>
